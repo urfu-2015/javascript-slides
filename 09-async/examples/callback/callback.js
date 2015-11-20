8808a5fffdd3fa03912fa973790a20c4ef3592fe
Sergey Zhigalov <zhigalov@yandex-team.ru>
2015-11-20 15:45:33 +0500

    Доделал часть про callback

---
+++ 09-async/examples/callback/callback.js
@@ -0,0 +1,22 @@
+var fs = require('fs');
+function (cb) {
+    var otherData;
+
+    fs.readFile(__dirname + '/first.txt', 'utf-8', function (err, data) {
+        err && cb(err);
+        if (otherData) {
+            cb(null, data, otherData);
+        } else {
+            otherData = data;
+        }
+    });
+
+    fs.readFile(__dirname + '/second.txt', 'utf-8', function (err, data) {
+        err && cb(err);
+        if (otherData) {
+            cb(null, otherData, data);
+        } else {
+            otherData = data;
+        }
+    });
+}
